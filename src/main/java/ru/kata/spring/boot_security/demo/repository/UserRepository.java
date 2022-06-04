1b5badef6b4012b47b3eeb5a53a157a63409337a
AleksandrShif <shifrin91@gmail.com>
2022-06-05 01:01:36 +0500

    F

---
+++ src/main/java/ru/kata/spring/boot_security/demo/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package ru.kata.spring.boot_security.demo.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import ru.kata.spring.boot_security.demo.entity.User;
+
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("SELECT u FROM User u WHERE u.id = :id")
+    public User getUserById(@Param("id") long id);
+
+    @Query("SELECT u FROM User u WHERE u.username = :username")
+    User getUserByUsername(@Param("username") String username);
+}
